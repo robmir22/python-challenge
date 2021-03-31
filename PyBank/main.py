bf5412894e48d04d9405382d0cad9a776435f481
robmir <roberto.miranda.gomez@gmail.com>
2021-03-30 20:13:36 -0600

    Finished PyBank with txt file

--- PyBank/main.py
+++ PyBank/main.py
@@ -56,3 +56,17 @@ print(f"Greatest Increase in Profits: {maxmonth} with ${max(changes_list)}")
 print(f"Greatest Decrease in Profits: {minmonth} with ${min(changes_list)}")
 print("--------------------------------------------------------")
 
+
+output_path = os.path.join( "analysis", "Financial_analysis.txt")
+
+with open(output_path, 'w') as result:
+
+    result.write("--------------------------------------------------------")
+    result.write("\nFinancial Analysis")
+    result.write("\n--------------------------------------------------------")
+    result.write(f"\nTotal Months: {month_count}")
+    result.write(f"\nTotal: $ {total}")
+    result.write(f"\nAverage  Change: $ {average_change:.2f}")
+    result.write(f"\nGreatest Increase in Profits: {maxmonth} with ${max(changes_list)}")
+    result.write(f"\nGreatest Decrease in Profits: {minmonth} with ${min(changes_list)}")
+    result.write("\n--------------------------------------------------------")

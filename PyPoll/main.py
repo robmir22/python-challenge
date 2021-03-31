029fb6ddc267572080c5c03e13d5022f981d2189
robmir <roberto.miranda.gomez@gmail.com>
2021-03-30 21:12:36 -0600

    Including winner in PyPoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -24,7 +24,10 @@ with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     csv_header = next(csvreader)
     
-    vote_count  = [0,0,0,0]
+    vote_count  = []
+    for x in range(len(candidates)):
+        vote_count.append(0)
+
 
     for vote in csvreader:
         if  str(vote[2]) == str(candidates[0]):
@@ -36,31 +39,24 @@ with open(csvpath) as csvfile:
         if  str(vote[2]) == str(candidates[3]):
             vote_count[3] += 1
     
-
-#The winner of the election based on popular vote.
-#In addition, your final script should both print the analysis to the terminal and export a text file with the results.
 percentages = []
 for x in range(len(candidates)):
-    percentages.append((vote_count[x])/casted_votes)
+    percentages.append((vote_count[x])/casted_votes*100)
 
-print("-------------------------")
+winner = max(vote_count)
+winner_position = vote_count.index(winner)
+winnerwinner = candidates[winner_position]
+
+
+print("------------------------------")
 print("Election Results")
-print("-------------------------")
+print("------------------------------")
 print(f"Total Votes: {casted_votes}")
-print("-------------------------")
-print(candidates)
-print(vote_count)
-
-
+print("------------------------------")
 for x in range(len(candidates)):
-    print(f"{candidates[x]} : ({vote_count[x]})")
-
-#Khan: 63.000% (2218231)
-#Correy: 20.000% (704200)
-#Li: 14.000% (492940)
-#O'Tooley: 3.000% (105630)
-#-------------------------
-#Winner: Khan
-#-------------------------
+    print(f"{candidates[x]}: {percentages[x]:.2f}% ({vote_count[x]} votes)")
+print("------------------------------")
+print(f"The winner is: {winnerwinner}")
+print("------------------------------")
 
 
